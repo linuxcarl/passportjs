65e8e6b0f0b983443142f5b815c1a7b0f9b4bb88
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-06-11 02:52:01 -0500

    ejercicio de e como implementar el manejo de sesion

---
+++ handle-session/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const session = require('express-session');
+const port = 3001;
+const app =  express();
+
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: "keyboard cat"
+    })
+)
+
+app.get('/',(req, res) => {
+    req.session.count = req.session.count ? req.session.count + 1 : 1;
+    res.status(200).json({hello:'cookies', counter: req.session.count });
+})
+
+app.listen(port, () =>{
+    
+    console.log(`listen in the url: http://localhost:${port}`);
+})
