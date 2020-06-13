cf16f4b0f9db25bc6ca1f150c5131679dffb6c42
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-06-13 18:42:51 -0500

    se agrego el scopeValidationHandler para validar los permisos de usuarios y se implemento en las rutas de movies y userMovies

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -9,6 +9,7 @@ const {
 } = require('../utils/schemas/movies');
 
 const validationHandler = require('../utils/middleware/validationHandler');
+const scopesValidationHandler = require('../utils/middleware/scopesValidationHandler');
 
 const cacheResponse = require('../utils/cacheResponse');
 const {
@@ -25,7 +26,9 @@ function moviesApi(app) {
 
   const moviesService = new MoviesService();
 
-  router.get('/', passport.authenticate('jwt', { session: false }), async function(req, res, next) {
+  router.get('/', passport.authenticate('jwt', { session: false }),
+  scopesValidationHandler(['read:movies']),
+  async function(req, res, next) {
     cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
     const { tags } = req.query;
 
@@ -44,6 +47,7 @@ function moviesApi(app) {
   router.get(
     '/:movieId', 
     passport.authenticate('jwt', { session: false }),
+    scopesValidationHandler(['read:movies']),
     validationHandler({ movieId: movieIdSchema }, 'params'),
     async function(req, res, next) {
       cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
@@ -64,6 +68,7 @@ function moviesApi(app) {
 
   router.post('/', 
   passport.authenticate('jwt', { session: false }), 
+  scopesValidationHandler(['create:movies']),
   validationHandler(createMovieSchema), async function(
     req,
     res,
@@ -85,6 +90,7 @@ function moviesApi(app) {
   router.put(
     '/:movieId',
     passport.authenticate('jwt', { session: false }),
+    scopesValidationHandler(['update:movies']),
     validationHandler({ movieId: movieIdSchema }, 'params'),
     validationHandler(updateMovieSchema),
     async function(req, res, next) {
@@ -110,6 +116,7 @@ function moviesApi(app) {
   router.delete(
     '/:movieId',
     passport.authenticate('jwt', { session: false }),
+    scopesValidationHandler(['delete:movies']),
     validationHandler({ movieId: movieIdSchema }, 'params'),
     async function(req, res, next) {
       const { movieId } = req.params;
